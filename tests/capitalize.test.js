ddde9cb644b60b390df41d88c1448bc402a5a41d
Kumiho <enotmos@yandex.ru>
2024-09-13 19:59:34 +0300

    rename test to __tests__

--- tests/capitalize.test.js
+++
@@ -1,7 +0,0 @@
-import { strict as assert } from 'node:assert';
-import capitalize from '../src/capitalize.js';
-
-assert.strictEqual(capitalize(''), '');
-assert.strictEqual(capitalize('hello'), 'Hello');
-
-console.log('Все тесты пройдены!');
